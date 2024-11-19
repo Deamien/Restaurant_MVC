8dc7ee1f9ab6fc7a8946d686cfb5262108a810e4
Deamien <snejko96@gmail.com>
2024-11-19 16:00:05 +0100

    Add project files.

---
+++ LAB2_HT2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LAB2_HT2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

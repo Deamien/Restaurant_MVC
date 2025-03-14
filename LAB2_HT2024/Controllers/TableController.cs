87480cc1cc2b51a143e5422530c50c0bf6a40184
Deamien <snejko96@gmail.com>
2025-03-14 19:56:59 +0100

    Progress

--- LAB2_HT2024/Controllers/TableController.cs
+++ LAB2_HT2024/Controllers/TableController.cs
@@ -1,23 +1,30 @@
 ﻿using LAB2_HT2024.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace LAB2_HT2024.Controllers
 {
+    [Authorize]
     public class TableController : Controller
     {
         private readonly HttpClient _client;
 
-        private string baseUri = "https://localhost:7194";
+        private string baseUrl = "https://localhost:7194";
 
         public TableController(HttpClient client)
         {
             _client = client;
         }
-    
+
+
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var response = await _client.GetAsync($"{baseUri}api/Table");
+            var response = await _client.GetAsync($"{baseUrl}api/Table/all");
+
+            var token = HttpContext.Request.Cookies["jwtToken"];
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
 
             var json = await response.Content.ReadAsStringAsync();
 
@@ -25,5 +32,51 @@ namespace LAB2_HT2024.Controllers
 
             return View(tableList);
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int TableId)
+        {
+
+            var token = HttpContext.Request.Cookies["jwtToken"];
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _client.GetAsync($"{baseUrl}/api/Table/GetTableById/{TableId}");
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var table = JsonConvert.DeserializeObject<TableViewModel>(json);
+
+
+
+            return View(table);
+
+            [HttpPost]
+            public async Task<IActionResult> Delete(int TableId)
+            {
+
+
+
+            }
+
+            [HttpPut]
+            public async Task<IActionResult> Update(int TableId)
+            {
+
+
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Add()
+            {
+
+                var token = HttpContext.Request.Cookies["jwtToken"];
+                _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            }
+
+
+
+
+        }
     }
-}
